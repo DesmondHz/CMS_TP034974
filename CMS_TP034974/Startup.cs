5a7d2f8fab4ea4f1a43a9d3563babcd4f7e02a4f
desmond_hz <desmond29ming@gmail.com>
2017-03-31 09:12:14 +0800

    Add project files.

---
+++ CMS_TP034974/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMS_TP034974.Startup))]
+namespace CMS_TP034974
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
